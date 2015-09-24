ed0ca45dc85e767e2a62aaae037ab4ea72508551
skawafuchi <13loodySword@gmail.com>
2015-09-24 15:45:37 -0700

    Finish main page
    -Added background image and styled it
    -Adjusted image and personal description / styled it
    -Renamed pictures folder to images folder
    -Added javascript to make carousel elements clickable

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 var main = function(){
-	console.log("hi");
+	
+	//makes pictures in carousel clickable links 
+	$(".item").click(function(){
+		window.location = $(this).find("a").attr("href");		
+	});
+	
+	//makes cursor change to pointer when hovering over carousel pictures
+	$(".item").hover(function() {
+	    $(this).css('cursor','pointer');
+	}, function() {
+	    $(this).css('cursor','auto');
+		
+	});
 };
 
 $(document).ready(main);
