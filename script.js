6a8824e1a56bb78f163b5029496f0e7527a6f8d3
skawafuchi <13loodySword@gmail.com>
2015-09-25 19:46:00 -0700

    Made Projects Home Page
    Created projects home page
    Added/created thumbnails
    Edited 404 Page
    Added more style/edited footer
    Edited index home page

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
-var main = function(){
-	
-	//makes pictures in carousel clickable links 
-	$(".item").click(function(){
-		window.location = $(this).find("a").attr("href");		
+var main = function() {
+
+	// makes pictures in carousel clickable links
+	$(".item").click(function() {
+		window.location = $(this).find("a").attr("href");
 	});
-	
-	//makes cursor change to pointer when hovering over carousel pictures
+
+	// makes cursor change to pointer when hovering over carousel pictures
 	$(".item").hover(function() {
-	    $(this).css('cursor','pointer');
+		$(this).css('cursor', 'pointer');
 	}, function() {
-	    $(this).css('cursor','auto');
-		
+		$(this).css('cursor', 'auto');
 	});
+
+	// makes icons at bottom animate when hovered over
+	$(".icon").hover(function() {
+		$(this).animate({
+			opacity : '1.0'
+		}, 200);
+	}, function() {
+		$(this).animate({
+			opacity : '0.7'
+		});
+	});
+
+	$(".thumbnail").hover(function() {
+		$(this).find('.caption').slideDown(200); 
+	}, function() {
+		$(this).find('.caption').slideUp(200);
+	});
+
 };
 
 $(document).ready(main);
