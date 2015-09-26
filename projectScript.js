7c77f8584b31883b1ffb2e25b3af387d236c5bce
skawafuchi <13loodySword@gmail.com>
2015-09-26 16:32:16 -0700

    Started XML for reading into site
    Felt it would be easier for multiple pages

--- projectScript.js
+++ projectScript.js
@@ -1,17 +1,55 @@
-var main = function(){
-	
-	//makes pictures in carousel clickable links 
-	$(".item").click(function(){
-		window.location = $(this).find("a").attr("href");		
-	});
-	
-	//makes cursor change to pointer when hovering over carousel pictures
-	$(".item").hover(function() {
-	    $(this).css('cursor','pointer');
-	}, function() {
-	    $(this).css('cursor','auto');
-		
-	});
+var main = function() {
+	loadProjects();
+	//loadProjectsNoAjax();
 };
+
+function printErrorMsg(XMLHttpRequest, errorMessage, errorThrown) {
+	alert("LOAD FAILED!: " + errorMessage + ":" + errorThrown);
+}
+
+
+function insertProjects(data) {
+	console.log($(data).find("project").length);
+	var newHTML = "<div class =\"row\">";
+	var project = $(data).find("project");
+	for (i = 0; i < project.length; i++){
+		console.log(project.eq(i).find("name").text());
+		if (i%3 == 0){
+			newHTML += "</div><div class =\"row\">"
+		}
+	}
+	
+	
+	
+//	$(data).find("project").each(function() {
+//		var newHTML = "<div class=\"row\">";
+//
+//		// newHTML += "<div class=\"col-md-2\">";
+//		// newHTML += "Title: " + $(this).find("title").text() + "<br>";
+//		// newHTML += "Published: " + $(this).find("pubDate").text()
+//		// + "<br>";
+//		// newHTML += "Latitude: " + $(this).find("geo\\:lat,lat").text()
+//		// + "<br>";
+//		// newHTML += "Longitude: "
+//		// + $(this).find("geo\\:long,long").text();
+//		// newHTML += "</div>";
+//		//
+//		// newHTML += "<div class=\"col-md-2\">";
+//		// newHTML += $(this).find("description").text();
+//		// newHTML += "</div>";
+//		//
+//		newHTML += "HI WAT UP</div>";
+//		$("#projects").append(newHTML);
+//	});
+}
+
+function loadProjects() {
+	$.ajax({
+		url : "projects.xml",
+		dataType : "xml",
+		success : insertProjects,
+		error : printErrorMsg
+	});
+}
 
 $(document).ready(main);
