0961e8b650b2be07cf44968066d45d4b9d7b57f3
skawafuchi <13loodySword@gmail.com>
2015-09-28 14:38:44 -0700

    Removed Template; Added to script

--- script.js
+++ script.js
@@ -24,4 +24,53 @@ var main = function() {
 	});
 };
 
+function loadProject(projectName) {
+		$.ajax({
+			url : "projects.xml",
+			dataType : "xml",
+			success : function(xml){
+				var projectData = $(xml).find('project[name="' + projectName + '"]');
+				$("#name").text(projectData.find("name").text());
+				if (projectData.find("time_frame").length){
+					var start_date = projectData.find("start_date");
+					var end_date =  projectData.find("end_date");
+					var time_period = start_date.find("month").text() + " " + start_date.find("year").text() + " - " + end_date.find("month").text() + " " + end_date.find("year").text();
+					$("#time_period").text(time_period);
+				}												
+	
+				$("#project_description").text(projectData.find("full_description").text());
+
+				projectData.find("member").each(function(){
+					$("#team_members").append($(this).text() + "<br>");
+				});
+				
+				if (projectData.find("note").length){
+					$("#note").html('<div class="alert alert-danger" role="alert">' + projectData.find("note").text()+'</div>');
+				}
+				
+				if (projectData.find("github_url").length){
+					$("#links").append('<a href="' + projectData.find("github_url").text() + '" class="label label-info"><span class="glyphicon glyphicon-pencil"></span>Code on Github</a>');
+				}
+				if (projectData.find("download_url").length){
+					$("#links").append('<a href="' + projectData.find("download_url").text() + '" class="label label-info"><span class="glyphicon glyphicon-download"></span>Download Project</a>');
+				}
+				if (projectData.find("play_url").length){
+					$("#links").append('<a href="' + projectData.find("play_url").text() + '" class="label label-info"><span class="glyphicon glyphicon-screenshot"></span>Play Online!</a>');
+				}
+
+				projectData.find('tag[type="technology"]').each(function(){
+					$("#technologies").append($(this).text() + "<br>");
+				});
+				
+				if(projectData.find("youtube_url").length){
+					$("#media").append('<iframe width="420" height="315" src="http://www.youtube.com/embed/' +projectData.find("youtube_url").text()  + '" style="margin-top:20px;"></iframe><br>');
+				}else{
+					$("#media").append('<img class="project-image" src="' +projectData.find("urls").find("thumbnail_url").text()+ '" style="margin-top:20px; width:420px; height: 315px;">');
+				}
+
+			}
+		});
+	}
+
+
 $(document).ready(main);
