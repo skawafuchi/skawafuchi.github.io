7da16820525174cdd55edf56a76f6e11b8bd07ef
skawafuchi <13loodySword@gmail.com>
2015-09-28 20:45:40 -0700

    Added Filter for Projects Page
    Edited GotA HTML to be fullscreen
    Added blog.html basic page
    Fixed URL for JKT

--- projectScript.js
+++ projectScript.js
@@ -1,49 +1,84 @@
 //TODO add filters
-
+var options = [ "Website", "Game" ];
 var main = function() {
 	loadProjects();
 
 	$(".thumbnail").hover(function() {
-		$(this).find('.caption').slideDown(200); 
+		$(this).find('.caption').slideDown(200);
 	}, function() {
 		$(this).find('.caption').slideUp(200);
 	});
-	
-	//Phone transitions
-	$(".thumbnail").on("tap",function() {
-		$(this).find('.caption').slideDown(200); 
+
+	// Phone transitions
+	$(".thumbnail").on("tap", function() {
+		$(this).find('.caption').slideDown(200);
 
 	});
-	
-	$(".caption").on("tap",function() {
-		$(this).slideUp(200); 
+
+	$(".caption").on("tap", function() {
+		$(this).slideUp(200);
 	});
+
+	$('.dropdown-menu a').on(
+			'click',
+			function(event) {
+
+				var $target = $(event.currentTarget), val = $target
+						.attr('data-value'), $inp = $target.find('input'), idx;
+
+				if ((idx = options.indexOf(val)) > -1) {
+					options.splice(idx, 1);
+					setTimeout(function() {
+						$inp.prop('checked', false)
+					}, 0);
+				} else {
+					options.push(val);
+					setTimeout(function() {
+						$inp.prop('checked', true)
+					}, 0);
+				}
+
+				$(event.target).blur();
+				console.log(options);
+				console.log("Game is in the array is: "+ ($.inArray("Game", options) === -1? "false":"true"));
+				console.log("Website is in the array is: "+ ($.inArray("Website", options) === -1?"false":"true"));
+				loadProjects();
+				return false;
+			});
 };
 
 function printErrorMsg(XMLHttpRequest, errorMessage, errorThrown) {
 	alert("LOAD FAILED!: " + errorMessage + ":" + errorThrown);
 }
 
-
 function insertProjects(data) {
 	var newHTML = "<div class =\"row\">";
 	var project = $(data).find("project");
-	for (i = 0; i < project.length; i++){
-		console.log(project.eq(i).find("name").text());
-		if (i%3 == 0 && i != 0){
-			newHTML += "</div><div class =\"row\">";
+	var projectsAdded = 0;
+	for (i = 0; i < project.length; i++) {
+		if (($.inArray("Game", options) !== -1 && project.eq(i).find('tag[type="filter"]').text() === "Game") ||
+				($.inArray("Website", options) !== -1 && project.eq(i).find('tag[type="filter"]').text() === "Website")) {
+			if (projectsAdded % 3 == 0 && i != 0) {
+				newHTML += "</div><div class =\"row\">";
+			}
+			newHTML += "<div class=\"col-md-4\">";
+			newHTML += "<h3>" + project.eq(i).find("name").text() + "</h3>";
+			newHTML += "<div class=\"thumbnail\">";
+			newHTML += "<div class=\"caption\" style=\"display: none;\">";
+			newHTML += "<p>"
+					+ project.eq(i).find("short_description").text()
+					+ "<br><br> <a href=\""
+					+ project.eq(i).find("urls").find("project_url").text()
+					+ "\" class=\"label label-info\" style=\"font-size: 20px;\">Visit Page</a></p>";
+			newHTML += "</div>";// END CAPTION
+			newHTML += "<img class=\"project-image\" src=\""
+					+ project.eq(i).find("urls").find("thumbnail_url").text()
+					+ "\">";
+			newHTML += "</div></div>"; // END THUMBNAIL && END COL
+			projectsAdded++;
 		}
-		newHTML += "<div class=\"col-md-4\">";
-		newHTML += "<h3>" + project.eq(i).find("name").text() + "</h3>";
-		newHTML += "<div class=\"thumbnail\">";
-		newHTML += "<div class=\"caption\" style=\"display: none;\">";
-		newHTML += "<p>" + project.eq(i).find("short_description").text() + "<br><br> <a href=\"" + project.eq(i).find("urls").find("project_url").text() + "\" class=\"label label-info\" style=\"font-size: 20px;\">Visit Page</a></p>";
-		newHTML += "</div>";//END CAPTION
-		newHTML += "<img class=\"project-image\" src=\"" +project.eq(i).find("urls").find("thumbnail_url").text()+ "\">";
-		newHTML += "</div></div>"; //END THUMBNAIL && END COL		
 	}
-	$("#projects").append(newHTML);
-	console.log("finished inserting projects");
+	$("#projects").html(newHTML);
 }
 
 function loadProjects() {
