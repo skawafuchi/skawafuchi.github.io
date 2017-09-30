1bb947c46dd916f061260f2a70b860ff99e5f9c9
skawafuchi <skawafuchi@gmail.com>
2017-09-30 11:28:21 -0700

    Whoops
    Add the text pls

--- script.js
+++ script.js
@@ -116,10 +116,10 @@ function loadProject(projectName) {
 				if(projectData.find("design").length){
 					projectData.find("level").each(function(){
 						$("#design").append('<div class="row"><div class="col-md-5"><img class="level-image" src="' +projectData.find("level_image").text()+ '" style="margin-top:20px; margin-bottom:10px; width:100%; height: 315px;"></div>' + 
-						'<div class="col-md-7"><h3>' + projectData.find("level_title") + '</h3>' + 
-						'<p>' + projectData.find("level_description") + '</p>' + 
+						'<div class="col-md-7"><h3>' + projectData.find("level_title").text() + '</h3>' + 
+						'<p>' + projectData.find("level_description").text() + '</p>' + 
 						'<h3>Level Notes</h3>' + 
-						'<p>' + projectData.find("level_notes") + '</p></div></div>');
+						'<p>' + projectData.find("level_notes").text() + '</p></div></div>');
 					});					
 				}
 
